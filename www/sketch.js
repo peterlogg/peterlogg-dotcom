8a8d65459d114b83843281f80d9b016d882546a9
peterlogg <logg_peter@yahoo.co.uk>
2018-02-13 21:51:45 +0000

    adding some colour@

--- www/sketch.js
+++ www/sketch.js
@@ -1,12 +1,16 @@
 function setup() {
-  createCanvas(640, 480);
+  canvas = createCanvas(window.innerWidth, window.innerHeight);
+  frameRate(500);
 }
 
 function draw() {
+	noStroke()
+	var clicked = color(0.5 * 255 * cos(frameCount), 0.5 * 255 * sin(-1 * frameCount), 0.5 * 255 * sin(frameCount));
+	var notClicked = color(0.05 * 255 * sin(frameCount), 0.5 * 255 * sin(-0.01 * frameCount), 0.5 * 255 * sin(frameCount));
   if (mouseIsPressed) {
-    fill(0);
+    fill(clicked);
   } else {
-    fill(255);
+    fill(notClicked);
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  ellipse(mouseX, mouseY, 25 + 50 * sin(-0.01 * frameCount), 25 + 50 * cos(0.01 * frameCount));
 }
