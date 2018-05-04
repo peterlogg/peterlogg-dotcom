8c596f0ed415c8351a4023c5636935120c95296c
peterlogg <logg_peter@yahoo.co.uk>
2018-05-04 06:37:06 +0100

    the ultimate commitment

--- www/sketch.js
+++ www/sketch.js
@@ -5,12 +5,17 @@ function setup() {
 
 function draw() {
 	noStroke()
-	var clicked = color(0.5 * 255 * cos(frameCount), 0.5 * 255 * sin(-1 * frameCount), 0.5 * 255 * sin(frameCount));
-	var notClicked = color(0.05 * 255 * sin(frameCount), 0.5 * 255 * sin(-0.01 * frameCount), 0.5 * 255 * sin(frameCount));
+	var clicked = color(0, 0.5 * 255 * sin(-1 * frameCount), 0.5 * 255 * sin(frameCount));
+	var notClicked = color(0.5*255*sin(frameCount), 0.05 * tan(frameCount), -0.5 * 255 * sin(frameCount));
   if (mouseIsPressed) {
     fill(clicked);
+    ellipse(0.5*mouseX, 0.5*mouseY, 25 + 5 * sin(-0.01 * frameCount), 25 + 50 * cos(0.01 * frameCount));
+    ellipse(0.5*mouseX, -0.5*mouseY, 25 + 5 * sin(-0.01 * frameCount), 25 + 50 * cos(0.01 * frameCount));
+    ellipse(-0.5*mouseX, -0.5*mouseY, 25 + 5 * sin(-0.01 * frameCount), 25 + 50 * cos(0.01 * frameCount));
+    ellipse(-0.5*mouseX, 0.5*mouseY, 25 + 5 * sin(-0.01 * frameCount), 25 + 50 * cos(0.01 * frameCount));
   } else {
     fill(notClicked);
+    ellipse(mouseX, mouseY, 25 + 5 * sin(-0.01 * frameCount), 25 + 50 * cos(0.01 * frameCount));
   }
-  ellipse(mouseX, mouseY, 25 + 50 * sin(-0.01 * frameCount), 25 + 50 * cos(0.01 * frameCount));
+  
 }
